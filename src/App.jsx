87d7e3c5b5aaa6e20e7ac5715852be1a4046425c
mauryashrishti04-alt <mauryashrishti04@gmail.com>
2026-09-17 12:23:38 +0530

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,22 @@ function App(){
     return(
     <div>
         <h1>Today skill Lab  project</h1>
-        <ClassCard Track="Mern Stack"
+        <div style={{dispaly:"flex"}}>
+        <ClassCard Track="Mern Stack" 
         Trainer="Rajeev Sir"
-        Time="10:50 to 12:30"
-        Room={101}/>
+         Time="10:50 to 12:30"
+         Room={101}/>
+                    <br />
+           <ClassCard Track="Data Analysis" 
+        Trainer="Rohit Sir"
+         Time="10:50 to 12:30"
+         Room={102}/>
+                    <br />
+           <ClassCard Track="Spring Boot" 
+        Trainer="Lucky Sir"
+         Time="10:50 to 12:30"
+         Room={103}/>
+         </div>
     </div>
     );
 }
--- src/component/ClassCard.jsx
+++ src/component/ClassCard.jsx
@@ -1,7 +1,7 @@
 const ClassCard=(props)=>{
     return(
         <div className="class-card">
-            <h2>{props.track}</h2>
+            <h2>Track:{props.track}</h2>
             <p>Trainer:{props.Trainer}</p>
             <p>Time:{props.Time}</p>
             <p>Room:{props.Room}</p>
